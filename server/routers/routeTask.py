cc0796283683df793ac3d55bf12cdead9ff57652
tanaka-naoki-heavisidelab <tanakanaoki@heavisidelab.jp>
2023-09-04 11:52:11 +0900

    Get task

--- server/routers/routeTask.py
+++ server/routers/routeTask.py
@@ -1,20 +1,34 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from pydantic import parse_obj_as
+from typing import List
 from server.schemas.common import TaskBase
-from server.schemas.task import Task,TaskCreate
+from server.schemas.task import Task, TaskCreate, TaskRead
 from server.schemas.token import TokenData
 from server.services.toAuth import get_current_user
 from server.services.toTask import create
-from fastapi import APIRouter,Depends
+from server.models.task import Task as TaskModel
+from server.db.database import database
+
 
 router = APIRouter()
-@router.post("/create_task", response_model=Task)
-async def create_user(
-    task: TaskBase,
-    current_user: TokenData = Depends(get_current_user)
+
+
+@router.post("/task", response_model=Task)
+async def create_task(
+    task: TaskBase, current_user: TokenData = Depends(get_current_user)
 ):
     taskcreate = TaskCreate(
-        title=task.title, 
-        detail=task.detail, 
+        title=task.title,
+        detail=task.detail,
         end_time=task.end_time,
-        user_id=current_user.id
+        user_id=current_user.id,
     )
     return await create(taskcreate)
+
+
+@router.get("/task", response_model=List[TaskRead])
+async def get_task(current_user: TokenData = Depends(get_current_user)):
+    query = select([TaskModel]).where(TaskModel.user_id == current_user.id)
+    results = await database.fetch_all(query)
+    return [parse_obj_as(TaskRead, dict(result)) for result in results]
--- server/schemas/task.py
+++ server/schemas/task.py
@@ -1,8 +1,17 @@
 from server.schemas.common import TaskBase
+from datetime import datetime as PythonDateTime
+from typing import Optional
 
 
 class TaskCreate(TaskBase):
-    user_id:int
+    user_id: int
+
 
 class Task(TaskCreate):
-    id:int
+    id: int
+
+
+class TaskRead(TaskBase):
+    created_at: PythonDateTime
+    update_at: Optional[PythonDateTime]
+    user_id: int
